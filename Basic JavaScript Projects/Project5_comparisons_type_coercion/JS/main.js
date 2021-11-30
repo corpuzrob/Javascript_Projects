557292709754ada0a7802a74f0e21d510c338b4d
corpuzrob <93008995+corpuzrob@users.noreply.github.com>
2021-11-29 16:46:18 -0800

    Update Javascript Comparison Type Coercion

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,8 +1,16 @@
-// document.write(typeof "Banana");
-// document.write("<br>");
+function statements()   {
+    document.write("I've got the world on a " + typeof "Banana" + ".");
+    document.write("<br>");
 
-// document.write(typeof 3.141592654);
-// document.write("<br>");
+    document.write("Age is just a " + typeof 3.141592654 + ".");
+    document.write("<br>");
 
-// document.write("number" + 42)
-// document.write("<br>");
+    document.write("The answer to life, the universe, and everything is " + 42 + ".")
+    document.write("<br>");
+
+    document.write("To " + 3E455 + " and beyond.");
+    document.write("<br>");
+
+    document.write("Would infinity minus 2 infinities be " + -5E503 + "?");
+    document.write("<br>");
+}
