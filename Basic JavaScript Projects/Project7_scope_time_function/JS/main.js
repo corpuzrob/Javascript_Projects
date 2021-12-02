449a8b495e067968c4951057b6986defb72d3a37
corpuzrob <93008995+corpuzrob@users.noreply.github.com>
2021-12-01 22:22:03 -0800

    Update main.js
    Added more comments.

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,13 +1,13 @@
 var Test_Income = 223048;
 
-// function using global variable
+// Here is a function using global variable
 function Tax_Bracket_Function_Test() {
     var Tax_Bracket_Test;
     Tax_Bracket_Test = (Test_Income >= 400000) ? "will have their taxes raised.":"will NOT have their taxes raised.";
     document.getElementById("Tax_Bracket_Status_Test").innerHTML = "Someone whose income is less than $400000 " + Tax_Bracket_Test;
 }
 
-// function using a local variable
+// This is a function using a local variable.  Note that I also intentionally created an error by misspelling "function" and saw that the console pointed out a syntax error, as well as using the console to debug the next section of code (about horoscopes).
 function Tax_Bracket_Function() {
     var User_Income, Tax_Bracket;
     User_Income = document.getElementById("User_Income").value;
@@ -15,7 +15,7 @@ function Tax_Bracket_Function() {
     document.getElementById("Tax_Bracket_Status").innerHTML = "At an income of $" + User_Income + ", " + Tax_Bracket;
 }
 
-// if assignment (note that before I got to step 151, the else assignment, I had already researched and added else if and else statements here, so I'm going to assume that this can count as my else and else if assignments as well.  I did however re-write and run the examples provided in Example2.js just to practice them.  Please let me know if this is not ok.  Thanks.)
+// I wrote the following section of code for the if assignment (note that before I got to step 151, the else assignment, I had already researched and added else if and else statements here, so I'm going to assume that this can count as my else and else if assignments as well.  I did however re-write and run the examples provided in Example2.js just to practice them.  Please let me know if this is not ok.  Thanks.)
 function Horoscope() {
 
     var User_Birth_Month = document.getElementById("User_Birth_Month").value.toLowerCase();
