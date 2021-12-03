0a0e4f7f2e0be3d743bf676c4092238cce928114
corpuzrob <93008995+corpuzrob@users.noreply.github.com>
2021-12-02 16:59:54 -0800

    Slice and Search Methods

--- Basic JavaScript Projects/Project8_strings_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_strings_methods/JS/main.js
@@ -6,9 +6,30 @@ function full_Sentence()    {
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
 
-let text = 
-function text_Search()  {
-    var str = "ham"
-    var result = str.search(a)
-    document.getElementById("Green_Eggs_and_Ham").innerHTML = result;
+function slice_Method() {
+    var Original_Sentence = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?";
+    var Sentence_Fragment = Original_Sentence.slice(9,13);
+    document.getElementById("Slice").innerHTML = Sentence_Fragment;
+}
+
+// This is something I tried but couldn't get to work 
+// function Text_Search()  {
+//     // document.getElementById("Green_Eggs_and_Ham").innerHTML = string;
+//     var string = "Green eggs and ham";
+//     var result = string.search(User_Search);
+//     document.getElementById("Search_Result").innerHTML = result;
+// }
+
+// function Text_Search()  {
+//     // document.getElementById("Green_Eggs_and_Ham").innerHTML = string;
+//     var str = Green_Eggs_and_Ham;
+//     var result = str.includes("ham", 1);
+//     document.getElementById("Search_Result").innerHTML = result;
+// }
+
+function Text_Search()  {
+    // let text= "Green eggs and ham";
+    let text= Green_Eggs_and_Ham;
+    let position = text.search("and");
+    document.getElementById("Search_Result").innerHTML = position;
 }
